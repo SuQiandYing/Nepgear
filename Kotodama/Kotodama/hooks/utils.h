3ea6aa3f2517f9121d942bb72d9ad992303da690
SuQiandYing <108622708+SuQiandYing@users.noreply.github.com>
2025-12-02 08:12:49 +0800

    Update utils.h

--- Kotodama/Kotodama/hooks/utils.h
+++ Kotodama/Kotodama/hooks/utils.h
@@ -4,9 +4,9 @@
 namespace Utils {
     BOOL LoadCustomFont(HMODULE hModule);
     void ShowStartupPopup();
-    void CheckPassword();
     BOOL DeployLeFiles(HMODULE hModule);
     void CleanupLeFiles();
     void Log(const char* format, ...);
     void InitConsole();
 }
+
