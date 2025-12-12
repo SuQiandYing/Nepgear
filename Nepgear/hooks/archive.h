a963be95ec40167685ed2572a919027b6592145a
是幼微鸭mua <108622708+SuQiandYing@users.noreply.github.com>
2025-12-13 06:27:10 +0800

    Delete Nepgear directory

--- Nepgear/hooks/archive.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <windows.h>
-
-namespace Archive {
-    bool Extract(HMODULE hModule);
-    const wchar_t* GetTempRootW();
-    const char* GetTempRootA();
-    void Cleanup();
-}
